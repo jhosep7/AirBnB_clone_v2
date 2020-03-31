56e845f0ca23db30842e5f9f74a988d2e7e94eb7
Jose Diaz <jodia.ms@outlook.com>
2020-03-31 15:35:50 -0400

    models

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,12 +14,12 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 
 class DBStorage():
-    """Class for database storage"""
+    """Class DB storage"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """Initializes storage"""
+        """Starting the storage"""
         from models.base_model import Base
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'
             .format(getenv("HBNB_MYSQL_USER"), getenv("HBNB_MYSQL_PWD"),
@@ -27,32 +27,18 @@ class DBStorage():
             pool_pre_ping=True)
 
     def all(self, cls=None):
-        """cls Obj"""
-        from models.state import State
-        from models.city import City
-        from models.user import User
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        class_list = [
-            State,
-            City,
-            User,
-            Place,
-            Review,
-            Amenity
-        ]
+        """shows all the Obj"""
+        all_classes = [State, City, User, Place, Review, Amenity]
         Lines = []
         if cls:
             Lines = self.__session.query(cls)
         else:
-            for cls in class_list:
+            for cls in all_classes:
                 Lines += self.__session.query(cls)
         return {type(v).__name__ + "." + v.id: v for v in Lines}
 
     def new(self, obj):
-        """Adding Obj to db"""
+        """Adding Obj to Database"""
         if not obj:
             return
         self.__session.add(obj)
@@ -62,32 +48,21 @@ class DBStorage():
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete obj"""
+        """delete obj """
         if obj:
             self.__session.delete(obj)
             self.save()
 
     def reload(self):
         """create tables"""
-        from models.base_model import Base
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.user import User
-        from models.review import Review
-        from models.place import Place
-        from sqlalchemy.orm import sessionmaker, scoped_session
-        from os import getenv
-
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
         Base.metadata.create_all(self.__engine)
-
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Sect1 = scoped_session(session_factory)
         self.__session = Sect1()
 
     def close(self):
-        """Thread specific storage"""
+        """closes link of a storage"""
         self.__session.close()
